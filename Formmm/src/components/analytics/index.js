143a96381c5a9091a7deb5bb9725903078863d84
ABHIJIT8008 <133222504+ABHIJIT8008@users.noreply.github.com>
2025-07-04 15:25:06 +0530

    Delete Formmm directory

--- Formmm/src/components/analytics/index.js
+++
@@ -1,6 +0,0 @@
-export { AnalyticsHeader } from './AnalyticsHeader';
-export { FormSelector } from './FormSelector';
-export { QuestionList } from './QuestionList';
-export { ResponseCharts } from './ResponseCharts';
-export { DetailedResponsesTable } from './DetailedResponsesTable';
-export { RespondentList } from './RespondentList';
--- Formmm/src/components/layout/MainLayout.jsx
+++
@@ -1,9 +0,0 @@
-import { Box, Flex } from '@chakra-ui/react';
-
-export const MainLayout = ({ children }) => {
-  return (
-    <Box minHeight="100vh" bg="#f8fafc">
-      {children}
-    </Box>
-  );
-};
